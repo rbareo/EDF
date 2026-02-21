704675504b0d90c249ccb3e89e5bc6a4e62bceb1
deoxycation <rbareo25@gmail.com>
2026-02-21 03:04:53 -0500

    Basic AST class structure

--- src/includes/AST.hh
+++ src/includes/AST.hh
@@ -4,6 +4,8 @@
 
 namespace SDN::Types::AST {
   enum class Kinds : short;
+  enum class Types : short;
+  enum class Operators : short;
 
   class Base;
   class Identifier;
@@ -13,24 +15,51 @@ namespace SDN::Types::AST {
 
 namespace AST = SDN::Types::AST;
 
-enum class AST::Kinds {
+enum class AST::Kinds : short {
   IDENTIFIER,
   LITERAL,
   BINARY_OPERATOR
 };
 
+enum class AST::Types : short {
+};
+
+enum class AST::Operators : short {
+};
+
 class AST::Base {
-  private:
+  public:
+    const AST::Kinds kind;
+
+    Base(AST::Kinds kind) : kind(kind) {};
+    ~Base() = default;
 };
 
 class AST::Identifier : public AST::Base {
-  private:
+  public:
+    std::string_view name;
+    AST::Types type;
+
+    Identifier(std::string_view name, AST::Types type) : Base(AST::Kinds::IDENTIFIER), name(name), type(type) {};
+    ~Identifier() = default;
 };
 
 class AST::Literal : public AST::Base {
-  private:
+  public:
+    std::string_view value;
+    AST::Types type;
+
+    Literal(std::string_view value, AST::Types type) : Base(AST::Kinds::LITERAL), value(value), type(type) {};
+    ~Literal() = default;
 };
 
 class AST::Binary_operator : public AST::Base {
-  private:
+  public:
+    const std::unique_ptr<AST::Base> left;
+    const std::unique_ptr<AST::Base> right;
+    const AST::Operators op;
+
+    Binary_operator(std::unique_ptr<AST::Base> left, std::unique_ptr<AST::Base> right, AST::Operators op)
+      : Base(AST::Kinds::BINARY_OPERATOR), left(std::move(left)), right(std::move(right)), op(op) {};
+    ~Binary_operator() = default;
 };
